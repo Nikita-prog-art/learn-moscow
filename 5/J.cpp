a5d65f9dcb7c40460c93a010e98ff647332fe5ea
Nikita-prog-art <xanext@mail.ru>
2022-04-01 16:45:24 +0500

    5 - J

--- 5/J.cpp
+++ 5/J.cpp
@@ -2,17 +2,54 @@
 #include <vector>
 #include <algorithm>
 
+#define n 5
+
 using namespace std;
 
 int main(){
-    int n, k;
-    cin >> n >> k;
-    vector <int> arr(n);
-    for (int &x : arr)
-        cin >> x;
-    int l = 0, r = k - 1;
-    while(r < n){
-        cout << *min_element(arr.begin() + l, arr.begin() + r + 1) << "\n";
-        l++, r++;
+    vector <int> l(5), r(5);
+    for (int i = 0; i < n; ++i)
+        cin >> l[i];
+    for (int i = 0; i < n; ++i)
+        cin >> r[i];
+    int cnt = 0;
+    while(!l.empty() && !r.empty() && cnt < 1e6){
+        if(l.back() == 0 && r.back() == 9){
+            l.insert(l.begin(), l.back());
+            l.insert(l.begin(), r.back());
+            l.pop_back();
+            r.pop_back();
+        }
+        else if(l.back() == 9 && r.back() == 0){
+            r.insert(r.begin(), l.back());
+            r.insert(r.begin(), r.back());
+            l.pop_back();
+            r.pop_back();
+        }
+        else if(l.back() > r.back()){
+            l.insert(l.begin(), l.back());
+            l.insert(l.begin(), r.back());
+            l.pop_back();
+            r.pop_back();
+        }
+        else if(r.back() > l.back()){
+            r.insert(r.begin(), l.back());
+            r.insert(r.begin(), r.back());
+            l.pop_back();
+            r.pop_back();
+        }
+        cnt++;
+    }
+    if (cnt == 1e6){
+        cout << "botva" << " " << cnt;
+        return 0;
+    }
+    if (l.empty()){
+        cout << "second" << " " << cnt;
+        return 0;
+    }
+    if (r.empty()){
+        cout << "first" << " " << cnt;
+        return 0;
     }
 }
